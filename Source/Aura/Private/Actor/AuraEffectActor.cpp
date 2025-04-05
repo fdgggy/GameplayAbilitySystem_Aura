b1f686c239d90b782567649e14a61b39f918f216
zhoujian <zhoujian@ZHOUJIAN>
2025-04-05 20:38:24 +0800

    属性

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,22 @@
+// Copyright banma
+
+
+#include "Actor/AuraEffectActor.h"
+
+// Sets default values
+AAuraEffectActor::AAuraEffectActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
